d8ea3067882a7752374b4005ff417f4195774bff
Anup-Shrestha <anupshresth9@gmail.com>
2020-07-07 20:28:06 +0545

    bh

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(){
+        System.out.println("reached");
+        return "Reached";
+    }
+}
